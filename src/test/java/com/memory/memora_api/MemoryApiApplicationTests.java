9160aa3e4b9043eb08f5fe15aefaef17e9b0121b
Elisson Douglas <97467970+ElissonDouglas@users.noreply.github.com>
2026-09-06 10:00:46 -0300

    add: dummy-test-key for gemini-api-key

--- src/test/java/com/memory/memora_api/MemoryApiApplicationTests.java
+++ src/test/java/com/memory/memora_api/MemoryApiApplicationTests.java
@@ -3,7 +3,11 @@ package com.memory.memora_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(properties = {
+		"gemini.api.key=dummy-test-key",
+		"spring.data.mongodb.uri=mongodb://localhost:27017/test_db"
+})
 class MemoryApiApplicationTests {
 
 	@Test
